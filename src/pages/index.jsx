c512bbbec0131bf65aa044f9897789f54945b2a6
kxdang <ikien.dang@gmail.com>
2020-02-02 16:36:31 -0500

    editing blog link to aboutme

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,7 +20,7 @@ const Index = props => {
       <p className="__intro">
         I'm a life-long learner with a passion for solving problems via code. I
         have a technical background in biochemistry and decided to make a career
-        change. I created a <a href="https://www.kiendang.me">blog</a> to
+        change. I created a <a href="https://www.kiendang.me/about">blog</a> to
         document my journey and progress; feel free to take a peak!
       </p>
     </div>
