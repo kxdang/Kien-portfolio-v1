346e8c6286a63c7635cda99e848af1fc79d5b0c8
kxdang <ikien.dang@gmail.com>
2020-03-15 12:47:21 -0400

    adding todo project

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -14,6 +14,27 @@ const ProjectList = () => (
     <tr className="table_light">
       <td>2020</td>
       <td>
+        <a href="https://kien-react-todo.netlify.com/" target="_blank">
+          <FaExternalLinkSquareAlt />
+          Simple Todo List
+        </a>
+      </td>
+      <td>React</td>
+      <td>
+        {" "}
+        <a
+          href="https://github.com/kxdang/LottoMin"
+          target="_blank"
+          className="icon"
+        >
+          <FaGithub />
+        </a>
+      </td>
+    </tr>
+
+    <tr className="table_light">
+      <td>2020</td>
+      <td>
         <a href="https://kien-lotto-min.netlify.com/" target="_blank">
           <FaExternalLinkSquareAlt />
           LottoMin
