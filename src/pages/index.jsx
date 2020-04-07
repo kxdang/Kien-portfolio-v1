155905831e34e17429827f3b706210ad004eb018
kxdang <ikien.dang@gmail.com>
2020-04-07 14:11:04 -0400

    removing page transition from the index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,60 +1,59 @@
 import React from "react";
 import Avatar from "../components/Avatar";
 import { FaLinkedin, FaGithubSquare, FaCanadianMapleLeaf } from "react-icons/fa";
-import PageTransition from 'gatsby-plugin-page-transitions';
 
 import HeadlineWithFocus from "../components/HeadlineWithFocus";
 
 const Index = props => {
 
   return (
-    <PageTransition transitionTime={200}>
-      <div className="c-content-box">
-        <Avatar modifierClasses="c-avatar--as-logo" />
 
-        <h2>Hello World, My name is Kien</h2>
-        <HeadlineWithFocus
-          modifierClassNames={`
+    <div className="c-content-box">
+      <Avatar modifierClasses="c-avatar--as-logo" />
+
+      <h2>Hello World, My name is Kien</h2>
+      <HeadlineWithFocus
+        modifierClassNames={`
           c-headline-focus 
           c-headline-focus--super-big 
         `}
-        >
-          I'm a front-end web developer.
+      >
+        I'm a front-end web developer.
       </HeadlineWithFocus>
-        <div className="c-content-box__contact">
-          <div className="c-content-box__contact--logo">
-            <a
-              href="https://www.linkedin.com/in/kien-dang/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaLinkedin />
-            </a>
-            <a
-              href="https://www.kiendang.me/about"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaCanadianMapleLeaf id="leaf" />
-            </a>
-            <a
-              href="https://github.com/kxdang"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaGithubSquare />
-            </a>
-          </div>
-
+      <div className="c-content-box__contact">
+        <div className="c-content-box__contact--logo">
+          <a
+            href="https://www.linkedin.com/in/kien-dang/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://www.kiendang.me/about"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaCanadianMapleLeaf id="leaf" />
+          </a>
+          <a
+            href="https://github.com/kxdang"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithubSquare />
+          </a>
         </div>
-        <p className="__intro">
-          I'm a life-long learner with a passion for solving problems via code. I
-          have a technical background in biochemistry and decided to make a career
+
+      </div>
+      <p className="__intro">
+        I'm a life-long learner with a passion for solving problems via code. I
+        have a technical background in biochemistry and decided to make a career
         change. I created a <a href="https://www.kiendang.me/about">blog</a> to
             document my journey and progress; feel free to take a peek!
       </p>
-      </div >
-    </PageTransition>
+    </div >
+
   );
 };
 
