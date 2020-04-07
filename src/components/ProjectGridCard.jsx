a12ba77e4aee0ab85d13c8dd887e7bb5868f9ce8
kxdang <ikien.dang@gmail.com>
2020-04-06 22:51:48 -0400

    editing cards and colours

--- src/components/ProjectGridCard.jsx
+++ src/components/ProjectGridCard.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { GoLinkExternal } from "react-icons/go";
+import { FaGithub } from "react-icons/fa";
 export default class ProjectGridCard extends Component {
     render() {
         return (
             <div className="ProjectGridCard">
                 <div className="ProjectGridCard-heading">
                     <h1>{this.props.name}</h1>
-                    <div className="ProjectGridCard-svg"><GoLinkExternal /></div>
+                    <div className="ProjectGridCard-Links"><FaGithub /><GoLinkExternal /></div>
                 </div>
 
             </div >
