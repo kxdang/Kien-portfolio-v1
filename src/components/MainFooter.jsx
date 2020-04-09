449ef7df144b285dc52e29aec454b09bb4e665e9
ikien.dang@gmail.com <ikien.dang@gmail.com>
2020-04-09 09:52:06 -0400

    removed footer to clean up

--- src/components/MainFooter.jsx
+++ src/components/MainFooter.jsx
@@ -3,29 +3,7 @@ import Link from "gatsby-link";
 
 const MainFooter = () => (
   <div className="c-main-footer">
-    <p className="c-main-footer__note">
-      Built with{" "}
-      <a
-        href="https://www.gatsbyjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Gatsby
-      </a>{" "}
-      and{" "}
-      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        React
-      </a>
-      . Hosted on{" "}
-      <a
-        href="https://www.netlify.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Netlify
-      </a>
-      .
-    </p>
+    <p className="c-main-footer__note"></p>
   </div>
 );
 
